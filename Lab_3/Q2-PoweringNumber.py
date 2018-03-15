0bda80fb71cd8e1b7e9909f645dc7c26ecaa538d
admin <admin@fsktm.edu>
2018-03-15 13:34:43 +0800

    Rename Q1 and add Q2

---
+++ Lab_3/Q2-PoweringNumber.py
@@ -0,0 +1,19 @@
+import math
+
+def powerNumber(number,power):
+    total = 0
+    if (power>=0):
+        #if power is even number
+        if(power%2 == 0):
+            print("even")
+            total = pow(number,(power/2)) * pow(number,(power/2))
+        else: #if power is odd
+            print("odd")
+            total = pow(number, ((power-1) / 2)) * pow(number, ((power-1) / 2)) * number
+        return total
+    else:
+        return "Power must be positive"
+
+
+
+print(powerNumber(10,3))
