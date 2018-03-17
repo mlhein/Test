11c75aa45c2ca4d4a7d00732c5b6a1a8277aa476
LIGHT-BEAST\Mlhein <clonm159@gmail.com>
2018-03-17 19:27:12 +0800

    Add Q4,Q5,Q6

---
+++ Lab_3/Q4-MatrixMultiplication.py
@@ -0,0 +1,26 @@
+def matMult (A, B):
+    rows_A = len(A)
+    cols_A = len(A[0])
+    rows_B = len(B)
+    cols_B = len(B[0])
+
+    if cols_A != rows_B:
+      print "Cannot multiply the two matrices. Incorrect dimensions."
+      return
+
+    # Create the result matrix
+    # Dimensions would be rows_A x cols_B
+    C = [[0 for row in range(cols_B)] for col in range(rows_A)]
+    print C
+
+    for i in range(rows_A):
+        for j in range(cols_B):
+            for k in range(cols_A):
+                C[i][j] += A[i][k] * B[k][j]
+    return C
+
+x = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+y = [[1,2],[1,2],[3,4]]
+
+print(matMult(y,y))
+
