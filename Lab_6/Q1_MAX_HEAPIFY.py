776a344af3b14570f4af2289c9d7f13ec2d2a62c
LIGHT-BEAST\Mlhein <clonm159@gmail.com>
2018-05-13 14:09:54 +0800

    reedit and complete

--- Lab_6/Q1_MAX_HEAPIFY.py
+++ Lab_6/Q1_MAX_HEAPIFY.py
@@ -1,51 +1,51 @@
+# Author: OMKAR PATHAK
 
-def max_heapify(A, i):
-    left = 2 * i + 1
-    right = 2 * i + 2
-    largest = i
-    if left < len(A) and A[left] > A[largest]:
-        largest = left
-    if right < len(A) and A[right] > A[largest]:
-        largest = right
-    if largest != i:
-        A[i], A[largest] = A[largest], A[i]
-        max_heapify(A, largest)
+#  Approach:
+#  Heap sort happens in two phases. In the first phase, the array
+#  is transformed into a heap. A heap is a binary tree where
+#  1) each node is greater than each of its children
+#  2) the tree is perfectly balanced
+#  3) all leaves are in the leftmost position available.
+#  In phase two the heap is continuously reduced to a sorted array:
+#  1) while the heap is not empty
+#  - remove the top of the head into an array
+#  - fix the heap.
 
-def build_max_heap(A):
-    for i in range(len(A) // 2, -1, -1):
-        max_heapify(A, i)
+#  Time Complexity of Solution:
+#  Best O(nlog(n)); Average O(nlog(n)); Worst O(nlog(n)).
 
-def heapSort(A):
-    build_max_heap(A)
-    for i in range(len(A) // 2):
-        A[1] = A[i]
-        len(A) = len(A) - 1
-        max_heapify(A, left)
+def HeapSort(alist):
+    heapify(alist)              # create the heap
+    end = len(alist) - 1
+    while end > 0:
+        alist[end], alist[0] = alist[0], alist[end]
+        shiftDown(alist, 0, end - 1)
+        end -= 1
 
-def heap_maximum(A):
-    return A[1]
+def heapify(alist):
+    ''' This function helps to maintain the heap property '''
+    # start = (len(alist) - 2) // 2         (faster execution)
+    start = len(alist) // 2
+    while start >= 0:
+        shiftDown(alist, start, len(alist) - 1)
+        start -= 1
 
-def heap_extract_max(A):
-    if len(A)< left:
-        print("Heap underflow")
-    max = A[1]
-    A[1] = A[len(A)]
-    len(A) = len(A) - 1
-    max_heapify(A, 1)
-    return max
+def shiftDown(alist, start, end):
+    root = start
+    while root * 2 + 1 <= end:
+        child = root * 2 + 1
+        # right child exists and is greater than left child
+        if child + 1 <= end and alist[child] < alist[child + 1]:
+            child += 1
+        # if child is greater than root(parent), then swap their positions
+        if child <= end and alist[root] < alist[child]:
+            alist[root], alist[child] = alist[child], alist[root]
+            root = child
+        else:
+            return
 
-def heap_increase_key(A, i, key):
-    if key < A[i]:
-        print("New key is smaller than current key")
-    A[i] =  key
-    while i > 1 and A[] <A[i]:
-
-def max_heap_insert(A, key):
-    len(A) = len(A) + 1
-    A[len(A)] = -
-    heap_increase_key(A, len(A), key)
-
-alist = ([11, 4, 74, 55, 3, 17, 8, 46, 43, 33])
-print("Unsorted array: ", alist)
-build_max_heap(alist)
-print('Sorted array: ', alist)
+if __name__ == '__main__':
+    alist = ([11, 4, 74, 55, 3, 17, 8, 46, 43, 33])
+    print("Unsorted array: ", alist)
+    HeapSort(alist)
+    print('Sorted Array:',alist)
